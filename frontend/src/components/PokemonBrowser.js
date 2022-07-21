cd02f897c2de4fc654ca2080a2db48c8da5bac46
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-07-21 12:59:51 -0700

    Finished phase 1

--- frontend/src/components/PokemonBrowser.js
+++ frontend/src/components/PokemonBrowser.js
@@ -1,18 +1,24 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
 
 import PokemonDetail from './PokemonDetail';
 import CreatePokemonForm from './CreatePokemonForm';
 import Fab from './Fab';
+import { getPokemon } from '../store/pokemon';
 
 const PokemonBrowser = () => {
+  const dispatch = useDispatch();
+
   const { pokemonId } = useParams();
   const pokemon = useSelector(state => {
     return state.pokemon.list.map(pokemonId => state.pokemon[pokemonId]);
   });
   const [showForm, setShowForm] = useState(false);
 
+  useEffect(() => {
+    dispatch(getPokemon())
+  }, [dispatch])
 
   if (!pokemon) {
     return null;
